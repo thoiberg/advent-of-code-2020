3b50542cfaf3c8a1427565defe5efca569279edc
Tim Hoiberg <tim.hoiberg@gmail.com>
2020-12-06 12:38:30 +1100

    Add part solution

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -1,3 +1,27 @@
+use std::io::Error as ioError;
+
 fn main() {
-    println!("Hello, world!");
+    let data = read_and_process_input().unwrap();
+    println!("{}", data.len());
+    println!("{}", data[0]);
+
+    // iterate through data
+    //  for each value iterate through the array again
+    // add both values together. If they match 2020 multiply them together
+    // and return result
+    for i in &data {
+        for j in &data {
+            if i + j == 2020 {
+                println!("Answer is: {}", i * j);
+            }
+        }
+    }
+}
+
+fn read_and_process_input() -> Result<Vec<i32>, ioError> {
+    let contents = include_str!("input_data");
+    Ok(contents
+        .split('\n')
+        .map(|val| val.parse::<i32>().unwrap())
+        .collect())
 }
