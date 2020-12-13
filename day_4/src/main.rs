93c47ddb133473dee023e42da44059438c5df736
Tim Hoiberg <tim.hoiberg@gmail.com>
2020-12-13 21:34:45 +1100

    Solution for Day 4 Part 1

--- day_4/src/main.rs
+++ day_4/src/main.rs
@@ -11,13 +11,77 @@ fn main() {
 }
 
 fn part_one_solution(passports: Vec<Vec<String>>) -> i32 {
-    0
+    // have a passport struct
+    // have all the values expressed as options properties
+    // add a new method that takes an array of strings and separates
+    //  them into k-v pairs
+    // have a match expression that the value of the property based on
+    //  the key. If it exists then set it as a Some, otherwise set it to None
+    let mut passport_objs: Vec<Passport> = vec![];
+
+    for passport in passports {
+        passport_objs.push(Passport::new(passport));
+    }
+
+    println!("Passport obj: {:?}", passport_objs[0]);
+
+    passport_objs.iter().fold(0, |acc, passport| {
+        if passport.clone().is_valid() {
+            return acc + 1;
+        }
+        acc
+    })
+}
+
+#[derive(Default, Debug, Clone)]
+struct Passport {
+    birth_year: Option<String>,
+    issue_year: Option<String>,
+    expiration_year: Option<String>,
+    height: Option<String>,
+    hair_color: Option<String>,
+    eye_color: Option<String>,
+    passport_id: Option<String>,
+    country_id: Option<String>,
+}
+
+impl Passport {
+    pub fn new(passport_data: Vec<String>) -> Passport {
+        let mut passport: Passport = Default::default();
+
+        for data in passport_data {
+            let key_value: Vec<&str> = data.split(':').collect();
+            let value = Some(key_value[1].to_string());
+
+            match key_value[0] {
+                "byr" => passport.birth_year = value,
+                "iyr" => passport.issue_year = value,
+                "eyr" => passport.expiration_year = value,
+                "hgt" => passport.height = value,
+                "hcl" => passport.hair_color = value,
+                "ecl" => passport.eye_color = value,
+                "pid" => passport.passport_id = value,
+                "cid" => passport.country_id = value,
+                _ => (),
+            }
+        }
+
+        passport
+    }
+
+    pub fn is_valid(self) -> bool {
+        self.birth_year.is_some()
+            && self.issue_year.is_some()
+            && self.expiration_year.is_some()
+            && self.height.is_some()
+            && self.hair_color.is_some()
+            && self.eye_color.is_some()
+            && self.passport_id.is_some()
+    }
 }
 
 fn read_input() -> Result<Vec<Vec<String>>, ioError> {
     let contents = include_str!("puzzle_data");
-    // need to break on \n\n to separate the passports then
-    // break on \n and whitespace to separate the data
     process_input(contents)
 }
 
@@ -48,5 +112,10 @@ mod tests {
         let example_data = test_data();
 
         assert_eq!(part_one_solution(example_data), 2);
+    }
+
+    #[test]
+    fn test_part_one_solution() {
+        assert_eq!(part_one_solution(read_input().unwrap()), 190);
     }
 }
